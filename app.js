151708f0d304fbd9da83730bdf8c3e4ff410af3f
ktrnaaa <120386793+ktrnaaa@users.noreply.github.com>
2024-02-01 20:54:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+import createError from 'createError';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+
+import './routes/index.js';
+import './routes/users.js';
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+
+export default app;
